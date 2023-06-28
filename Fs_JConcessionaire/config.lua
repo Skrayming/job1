fc544a13b01bf488a277c693915b826070467c61
Skrayming <126698084+Skrayming@users.noreply.github.com>
2023-06-28 20:42:14 +0200

    Add files via upload

---
+++ Fs_JConcessionaire/config.lua
@@ -0,0 +1,86 @@
+Concess             = {}
+
+Concess = {
+
+	webhooks = "",
+
+	jeveuxmarker = true, --- true = Oui | false = Non
+	blips1 = true, --- true = Oui | false = Non
+	blips2 = true, --- true = Oui | false = Non
+
+    ------------
+    ESXTrigger = "esx:getSharedObject",
+    ------------
+
+    ColorMenuR = 11, -- Bannière couleur R
+    ColorMenuG = 11, -- Bannière couleur G
+    ColorMenuB = 255, -- Bannière couleur B
+    ColorMenuA = 150, -- Bannière couleur A
+    MenuPositionX = 0,
+    MenuPositionY = 0,
+}
+
+Marker = {
+	Type = 6,
+	Color = {R = 255, G = 255, B = 255},
+	Size =  {x = 1.0, y = 1.0, z = 1.0},
+	DrawDistance = 10,
+	DrawInteract = 1.5,
+}
+
+Concess.pos = {
+
+	ascenceur = {
+		monter = {x = 138.47, y = -137.2, z = 54.86},
+		descendre = {x = 137.65, y = -134.27, z = 60.52}
+	},
+
+	blips = {
+		position = {x = -1573.9321289063, y = -960.17651367188, z = 14.003775596619}
+	},
+
+	blips2 = {
+		position = {x = -1648.7518310547, y = -994.39270019531, z = 13.017328262329}
+	},
+
+	vente = {
+		position = {x = -1556.076171875, y = -971.03289794922, z = 13.814208984375}
+	},
+
+	boss = {
+		position = {x = 123.01, y = -156.28, z = 60.49}
+	},
+
+	serrurier = {
+		position = {x = -1648.96484375, y = -994.20495605469, z = 13.017322540283}
+	},
+
+	catalogue = {
+		position = {x = -1560.0263671875, y = -975.44689941406, z = 13.814208984375}
+	},
+
+	showroom = {
+		menu = {x = -1554.3454589844, y = -959.05621337891, z = 13.814208984375},
+		show1 = {x = -1577.7266845703, y = -958.82757568359, z = 13.992756843567, h = 102.70928192138672},
+		show2 = {x = -1569.2310791016, y = -949.67785644531, z = 13.992762565613, h = 9.82960224151611},
+		show3 = {x = -1569.5374755859, y = -971.73052978516, z = 13.853610992432, h = 226.41929626464844,},
+
+	},
+	
+	viewvehicatalogue = {
+		position = {x = -1577.7266845703, y = -958.82757568359, z = 13.992756843567, h = 102.70928192138672}
+	},
+
+	posEssai = {
+		position = {x = -899.62, y = -3298.74, z = 13.94, h = 58.0}
+	},
+
+	coffre = {
+		position = {x = -613.46441650391, y = -1092.0582275391, z = 25.74457359314}
+	},
+
+	spawnvoiture = {
+		position = {x = -1578.4299316406, y = -939.18084716797, z = 13.017321586609, h = 132.90692138671875}
+	}
+
+}
